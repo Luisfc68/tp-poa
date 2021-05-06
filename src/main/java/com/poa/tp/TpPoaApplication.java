0576ff9281960eda070af78063583d38ea918a7f
Luisfc68 <Luisfc68@gmail.com>
2021-05-06 15:51:12 -0300

    Primer commit

---
+++ src/main/java/com/poa/tp/TpPoaApplication.java
@@ -0,0 +1,13 @@
+package com.poa.tp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpPoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpPoaApplication.class, args);
+	}
+
+}
